b7b44b97c1ccc25d163aa14e6683e6be95621c56
khushbu <khushboo931@gmail.com>
2024-02-05 23:59:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import axios from 'axios';
+import { useState } from 'react'
+const App = () => {
+    const[users, setusers]=useState([]);
+    const handleclick=()=>{
+        axios.get('https://65c11d2ddc74300bce8d561c.mockapi.io/users')
+        .then(response=>{
+            console.log(response.data);
+            setusers(response.data)
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+  return (
+    <>
+    <h2>Axios get method</h2>
+    <input type="button" value="Click me" onClick={handleclick}/>
+    {users.map((user)=>{return(
+        <div key={user.id}>
+            Name--{user.name}<br/>
+            Age--{user.age}
+        </div>
+    )})}
+    </>
+  )
+}
+
+export default App
